f4fb75ef82bb35a5ecd3727823e0f5404989e2ed
Terencio Agozzino <terencio.agozzino@gmail.com>
2017-09-20 08:16:13 +0200

    Add doc for Conf-IPv4

--- Conf-IPv4.ps1
+++ Conf-IPv4.ps1
@@ -1,24 +1,59 @@
-# This script is to make a basic IPv4 configuration for the server.
-#
-# You can verify your IP addresses configurations with:
-# `netsh interface ipv4 show addresses`
-#
-# You can verify your DNS server addresses configurations with:
-# `netsh interface ipv4 show dnsservers`
+<#
+.SYNOPSIS
+    Sets a basic IPv4 configuration with the possibility to change the hostname of the machine.
+    PowerSploit Function: Conf-IPv4
+    Author: Terencio Agozzino (@rememberYou)
+    License: None
+    Required Dependencies: None
+    Optional Dependencies: None
+    Version: 1.0.0
+ 
+.DESCRIPTION
+    Conf-IPv4 sets a basic IPv4 configuration with the possibility to change the hostname of the machine.    
+
+.EXAMPLE
+    PS C:\> Conf-IPv4 -Name SRVDNSPrimary -InterfaceIndex 5 -IP 172.16.0.0 -Length 24 -Gateway 172.16.0.0.1 -DnsSec 172.16.0.11
+
+.EXAMPLE
+    PS C:\> Conf-IPv4 -InterfaceIndex 5 -IP 172.16.0.0 -Length 24 -Gateway 172.16.0.0.1 -DnsSec 172.16.0.11 
+
+.NOTES
+    You can verify your IP addresses configurations with:
+    `netsh interface ipv4 show addresses`
+
+    You can verify your DNS server addresses configurations with:
+    `netsh interface ipv4 show dnsservers`
+#>
+
 Param(    
-    [string]$Name,
-    [string]$InterfaceIndex = "5",
-    [string]$IP = "172.16.120.10",
-    [string]$Length = "24",
-    [string]$Gateway = "172.16.120.1",
-    [string]$DnsSec = "172.16.120.11"
+    [String]
+    $Name,
+
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $InterfaceIndex,
+    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $IP,
+    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $Length,
+    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $Gateway,
+    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $DnsSec
 )
 
 # Use Get-NetIPInterface to get the InterfaceIndex.
 New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress $IP -PrefixLength $Length -DefaultGateway $Gateway
-Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses($IP, $DnsSec)
+Set-DnsClientServerAddress -InterfaceIndex 4 -ServerAddresses($IP, $DnsSec)
 
-If(-Not [string]::IsNullOrEmpty($Name))
-{
+If(-Not [string]::IsNullOrEmpty($Name)) {
     Rename-computer $Name
 }
