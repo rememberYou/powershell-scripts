c6876802efd62db2107da2be1f98490a80afc381
Terencio Agozzino <terencio.agozzino@gmail.com>
2017-09-20 08:57:01 +0200

    Add Args params for DNS

--- Conf-DNS.ps1
+++ Conf-DNS.ps1
@@ -1,32 +1,74 @@
-# This script is to make the DNS configuration for the server.
-#
-# You can verify the DNS installation with:
-# `Get-WindowsFeature`
-#
-# You can verify the DNS zones with:
-# `Get-DnsServerZone`
+<#
+.SYNOPSIS
+    Installs the DNS service and sets a basic DNS configuration.
+    PowerSploit Function: Conf-DNS
+    Author: Terencio Agozzino (@rememberYou)
+    License: None
+    Required Dependencies: None
+    Optional Dependencies: None
+    Version: 1.0.0
+ 
+.DESCRIPTION
+    Conf-DNS Installs the DNS service and sets a basic DNS configuration.
+
+.EXAMPLE
+    PS C:\> Conf-DNS -ZoneName heh.lan -ReplicationZone Domain 
+    -NetworkID 172.16.120.0 -Prefix 24 -RevZoneName 120.16.172.in-addr.arpa
+    -SRVPri SRVDNSPrimary -SRVSec SRVDNSSecondary
+
+.EXAMPLE
+    PS C:\> Conf-DNS -ZoneName heh.lan -ReplicationZone Domain 
+    -NetworkID 172.16.120.0 -Prefix 24 -RevZoneName 120.16.172.in-addr.arpa
+    -SRVPri SRVDNSPrimary -SRVSec SRVDNSSecondary
+
+.EXAMPLE
+    PS C:\> Conf-DNS -ZoneName heh.lan -NetworkID 172.16.120.0 -Prefix 24 
+    -RevZoneName 120.16.172.in-addr.arpa -SRVPri SRVDNSPrimary -SRVSec SRVDNSSecondary
+
+.NOTES
+    You can verify the DNS installation with:
+    `Get-WindowsFeature`
+
+    You can verify the DNS zones with:
+    `Get-DnsServerZone`
+#>
+
+Param(    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $ZoneName,
+        
+    [String]
+    $ReplicationZone=Domain
+    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $NetworkID,
+    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $Prefix,
+    
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $RevZoneName
+)
 
 Import-Module ServerManager
 Add-WindowsFeature -Name DNS -IncludeManagementTools
 
 # Create Forward Lookup Zones
-$ZoneName="heh.lan"
-Add-DnsServerPrimaryZone -Name "$ZoneName" -ZoneFile "$ZoneName.dns"
+Add-DnsServerPrimaryZone -Name "$ZoneName" -ZoneFile "$ZoneName.dns" -DynamicUpdate Secure -ReplicationZone "$ReplicationZone"
 
 # Create Reverse Lookup Zones
-$NetworkID="172.16.120.0"
-$Prefix="24"
-$RevZoneName="120.16.172.in-addr.arpa"
 Add-DnsServerPrimaryZone -NetworkID "$NetworkID/$Prefix" -ZoneFile "$RevZoneName.dns"
 
 # Create Records
-$SRVPrimary="SRVDNSPrimary"
-Add-DnsServerResourceRecordA -Name "$SRVPrimary" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.0.10" -CreatePtr
-Add-DnsServerResourceRecordAAAA -Name "$SRVPrimary" -ZoneName "$ZoneName" -AllowUpdateAny -IPv6Address "ACAD::10" -CreatePtr 
+Add-DnsServerResourceRecordA -Name "$SRVPri" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.120.10" -CreatePtr
+Add-DnsServerResourceRecordAAAA -Name "$SRVPri" -ZoneName "$ZoneName" -AllowUpdateAny -IPv6Address "ACAD::10" -CreatePtr 
 
-$SRVSecondary="SRVDNSSecondary"
-Add-DnsServerResourceRecordA -Name "$SRVSecondary" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.0.11" -CreatePtr
-Add-DnsServerResourceRecordAAAA -Name "$SRVSecondary" -ZoneName "$ZoneName" -AllowUpdateAny -IPv6Address "ACAD::11" -CreatePtr
+Add-DnsServerResourceRecordA -Name "$SRVSec" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.120.11" -CreatePtr
+Add-DnsServerResourceRecordAAAA -Name "$SRVSec" -ZoneName "$ZoneName" -AllowUpdateAny -IPv6Address "ACAD::11" -CreatePtr
 
 # Create Alias
-Add-DnsServerResourceRecordCName -Name "www" -HostNameAlias "$SRVPrimary.$ZoneName" -ZoneName "$ZoneName"
+Add-DnsServerResourceRecordCName -Name "www" -HostNameAlias "$SRVPri.$ZoneName" -ZoneName "$ZoneName"
