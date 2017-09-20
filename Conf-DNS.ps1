3d0cfd874c6c84f5f12dedc832203fd4f6b6fe64
Terencio Agozzino <terencio.agozzino@gmail.com>
2017-09-20 10:40:55 +0200

    Fix DNS Secondary

--- Conf-DNS.ps1
+++ Conf-DNS.ps1
@@ -12,9 +12,7 @@
     Conf-DNS Installs the DNS service and sets a basic DNS configuration.
 
 .EXAMPLE
-    PS C:\> Conf-DNS -ZoneName heh.lan 
-    -NetworkID 172.16.120.0 -Prefix 24 -RevZoneName 120.16.172.in-addr.arpa
-    -SRVPri SRVDNSPrimary -SRVSec SRVDNSSecondary
+    PS C:\> Conf-DNS -ZoneName heh.lan -NetworkID 172.16.0.0 -Prefix 16 -RevZoneName 16.172.in-addr.arpa -SRVPri SRVDNSPrimary -SRVSec SRVDNSSec
 
 .NOTES
     You can verify the DNS installation with:
@@ -27,7 +25,7 @@
 Param(    
     [ValidateNotNullOrEmpty()]
     [String]
-    $ZoneName,
+    $ZoneName,        
     
     [ValidateNotNullOrEmpty()]
     [String]
@@ -39,23 +37,31 @@ Param(
     
     [ValidateNotNullOrEmpty()]
     [String]
-    $RevZoneName
+    $RevZoneName,
+
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $SRVPri,
+
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $SRVSec
 )
 
 Import-Module ServerManager
 Add-WindowsFeature -Name DNS -IncludeManagementTools
 
 # Create Forward Lookup Zones
-Add-DnsServerPrimaryZone -Name "$ZoneName" -ZoneFile "$ZoneName.dns" -DynamicUpdate Secure
+Add-DnsServerPrimaryZone -Name "$ZoneName" -ZoneFile "$ZoneName.dns"
 
 # Create Reverse Lookup Zones
 Add-DnsServerPrimaryZone -NetworkID "$NetworkID/$Prefix" -ZoneFile "$RevZoneName.dns"
 
 # Create Records
-Add-DnsServerResourceRecordA -Name "$SRVPri" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.120.10" -CreatePtr
-Add-DnsServerResourceRecordAAAA -Name "$SRVPri" -ZoneName "$ZoneName" -AllowUpdateAny -IPv6Address "ACAD::10" -CreatePtr 
+Add-DnsServerResourceRecordA -Name "$SRVPri" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.0.10" -CreatePtr
+Add-DnsServerResourceRecordAAAA -Name "$SRVPri" -ZoneName "$ZoneName" -AllowUpdateAny -IPv6Address "ACAD::10" -CreatePtr
 
-Add-DnsServerResourceRecordA -Name "$SRVSec" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.120.11" -CreatePtr
+Add-DnsServerResourceRecordA -Name "$SRVSec" -ZoneName "$ZoneName" -AllowUpdateAny -IPv4Address "172.16.0.11" -CreatePtr
 Add-DnsServerResourceRecordAAAA -Name "$SRVSec" -ZoneName "$ZoneName" -AllowUpdateAny -IPv6Address "ACAD::11" -CreatePtr
 
 # Create Alias
