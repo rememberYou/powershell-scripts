905f91ec90698e9a0c20f8779e159f1fb45c9932
Terencio Agozzino <terencio.agozzino@gmail.com>
2017-09-30 21:49:25 +0200

    Add Windows SDK to Certificat

--- Certificats/Create-Cert.ps1
+++ Certificats/Create-Cert.ps1
@@ -24,11 +24,19 @@
 Add-WindowsFeature AD-Certificat `
 # Set-ExecutionPolicy AllSigned `
 
+# Download Windows SDK
+(Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/p/?linkid=845298").Links.Href
+
+# Install the Windows SDK
+.\winsdksetup.exe
+
+# Create a self-signed certificate.
 Set-Location "C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86"
 .\makecert.exe -n "CN=Powershell Local Certificate Root" -a sha1 `
   -eku 1.3.6.1.5.5.7.3.3 -r -sv root.pvk root.cer -ss Root -sr localMachine
 .\makecert.exe -pe -n "CN=Server PowerShell CSC" -ss MY -a sha1 `
   -eku 1.3.6.1.5.5.7.3.3 -iv root.pvk -ic root.cer
 
+# Now, you can sign your scripts like the following:
 # $acert =(dir Cert:\CurrentUser\My -CodeSigningCert)[0]
 # Set-AuthenticodeSignature .\foo.ps1 -Certificate $acert
