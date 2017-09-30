85bc81e6bf48095075c8c1d4d5abf84b486a4d19
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2017-09-30 21:44:35 +0200

    Add backup disk configuration

--- Backup and PSO/Conf-Backup.ps1
+++ Backup and PSO/Conf-Backup.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Configures the Windows Server Backup.
+    Configures the Windows Server Backup as well as the backup Disk.
     PowerSploit Function: Conf-Backup
     Author: Terencio Agozzino (@rememberYou)
             Alexandre Ducobu (@Harchytekt)
@@ -10,12 +10,18 @@
     Version: 1.0.0
 
 .DESCRIPTION
-    Conf-Backup Configures the Windows Server Backup.
+    Conf-Backup Configures the Windows Server Backup as well as the backup Disk.
 
 .EXAMPLE
-    PS C:\> Conf-Backup -Disk D
+    PS C:\> Conf-Backup -Disk E
+
+.EXAMPLE
+    PS C:\> Conf-Backup -Disk E -ConfigureDisk yes
 
 .NOTES
+    You can list all the available disks :
+    `get-disk`
+
     You can get a summary of previously run backup operations :
     `Get-WBSummary`
 
@@ -24,21 +30,32 @@
 Param(
     [ValidateNotNullOrEmpty()]
     [String]
-    $Disk
+    $Disk,
+
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $ConfigureDisk
 )
 
+If ($ConfigureDisk -ne $Null) {
+    # Configure the Backup disk
+    set-disk 1 -isOffline $false
+    Initialize-Disk -Number 1 -PartitionStyle MBR
+    New-Partition -DiskNumber 1 -UseMaximumSize -DriveLetter "$Disk"
+    Format-volume -DriveLetter "$Disk" -FileSystem NTFS
+}
 
 # Install Windows Server Backup
-Install-WindowsFeature -Name Windows-Server-Backup #-Restart:$false
-#Install-WindowsFeature -Name Windows-Server-Backup -IncludeAllSubfeature -IncludeManagementTools
+Install-WindowsFeature -Name Windows-Server-Backup -Restart:$false
 
+$WBPolicy = New-WBPolicy
 # Back up the System State
-Add-WBSystemState -Policy New-WBPolicy
+Add-WBSystemState -Policy $WBPolicy
 
 # Declare the backup location on the chosen Disk
 $target = New-WBBackupTarget -VolumePath "${Disk}:"
 
-Add-WBBackupTarget -Policy New-WBPolicy -Target $target
+Add-WBBackupTarget -Policy $WBPolicy -Target $target
 
 # Start the backup
-Start-WBBackup -Policy New-WBPolicy
+Start-WBBackup -Policy $WBPolicy
