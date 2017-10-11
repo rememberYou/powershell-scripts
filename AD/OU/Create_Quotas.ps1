ee9c970ac0de9dd1e7141d3674b6dd7a37dcac7b
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2017-10-11 17:18:41 +0200

    Add Quotas

---
+++ AD/OU/Create_Quotas.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+    Creates the quotas for all departments' shared folder.
+    PowerSploit Function: Create_Quotas
+    Author: Terencio Agozzino (@rememberYou)
+            Alexandre Ducobu (@Harchytekt)
+    License: None
+    Required Dependencies: None
+    Optional Dependencies: None
+    Version: 1.0.0
+
+.DESCRIPTION
+    Create_Quotas Creates the quotas for all departments' shared folder.
+
+.EXAMPLE
+    PS C:\> Create_Quotas -File C:\Users\Administrator\Desktop\ou-example.csv
+
+.NOTES
+    Take a look at the "ou-example.csv" file for the specific structure
+    and change it with yours.
+
+    You can verify the creation of your Organizatinal Units with:
+    `Get-ADOrganizationalUnit -Filter *`
+#>
+
+Install-WindowsFeature -Name FS-Resource-Manager, RSAT-FSRM-Mgmt
+
+# Create Templates
+New-FsrmQuotaTemplate -Name "500 MB limit" -Description "limit usage to 500 MB." -Size 500MB
+# New-FsrmQuotaTemplate -Name "100MB limit" -Description "limit usage to 100 MB." -Size 100MB
+
+
+
+# Apply template to the Shared Folder
+$Departments = gci -Directory "C:\Shared" | select name
+
+ForEach($folder in $Departments)
+{
+    $currentFolder = $folder.Name
+    New-FsrmQuota -Path "C:\Shared\$currentFolder" -Template "500 MB limit"
+    if ($currentFolder -ne "Common") {
+        $subDepartments = gci -Directory "C:\Shared\$currentFolder" | select name
+        ForEach ($folder in $subDepartments)
+        {
+            $currentSubFolder = $folder.Name
+            New-FsrmQuota -Path "C:\Shared\$currentFolder\$currentSubFolder" -Template "100 MB limit"
+        }
+    }
+}
