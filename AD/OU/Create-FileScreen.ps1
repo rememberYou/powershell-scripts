2f04b72e4f62167f9d1d521b72a554634e455ac8
Terencio Agozzino <terencio.agozzino@gmail.com>
2017-10-16 21:54:31 +0200

    Clean Create-FileScreen

--- AD/OU/Create-FileScreen.ps1
+++ AD/OU/Create-FileScreen.ps1
@@ -13,19 +13,21 @@
     Create-FileScreen Creates the file screen for the shared folder.
 #>
 
-
-
 # Create Action
-$logAction = New-FsrmAction Event -EventType Information -Body "An user attempted to create a forbidden file in the shared folder." -RunLimitInterval 60
+$logAction = New-FsrmAction Event -EventType Information `
+  -Body "An user attempted to create a forbidden file in the shared folder." `
+  -RunLimitInterval 60
 
 # Create File Group
 $allFileGroup = New-FsrmFileGroup -Name "All files" -IncludePattern "*"
 
 # Create File Screen Template
-$fsTemplate = New-FsrmFileScreenTemplate -Name "Deny All Type of Files" -IncludeGroup "All files" -Notification $logAction -Active
+$fsTemplate = New-FsrmFileScreenTemplate -Name "Deny All Type of Files" `
+  -IncludeGroup "All files" -Notification $logAction -Active
 
 # Set the File Screen on the shared folder
 New-FsrmFileScreen -Path "C:\Shared\" -Template "Deny All Type of Files"
 
 # Set the File Screen Exception on the shared folder
-New-FsrmFileScreenException -Path "C:\Shared\" -IncludeGroup "Image Files","Office Files"
+New-FsrmFileScreenException -Path "C:\Shared\" `
+  -IncludeGroup "Image Files", "Office Files"
